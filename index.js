038893d686c7d23d2ac8f764ccef337e1cc54fe5
athitharnch <athitharnch@playwork.co.th>
2019-01-22 13:23:25 +0700

    dwadawd

---
+++ index.js
@@ -0,0 +1,31 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb+srv://playwork:12345@cluster0-4z3vr.mongodb.net/link', {useNewUrlParser: true});
+// mongoose.connect('mongodb://localhost:27017/zztest', {useNewUrlParser: true} )
+
+// const Cat = mongoose.model('cat', { username: String, password: String });
+
+// Cat.find().then(s=>{
+//     console.log(s)
+// })
+
+// const cat = new Cat({ username: 'maw' })
+// cat.save().then(() => console.log('Kub'));
+
+// const kitty = new Cat({ name: 'Zildjian' });
+// kitty.save().then(() => console.log('meow'));
+
+
+// const user = mongoose.model('user', { username: String, password: String })
+
+// const uu = user.find()
+// console.log(uu)
+
+
+function abc (){
+    console.log('1111')
+}
+
+
+
+
